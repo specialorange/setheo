6440559d53ad581cbada97d442362c15b33a2001
Chris Frisina <github@specialorange.org>
2012-11-14 16:02:30 -0500

    branching to get about working

--- js/functions.js
+++ js/functions.js
@@ -83,72 +83,65 @@ jQuery(document).ready(function($) {
 	Portfolio 
    --------------------------------------------------------	*/	
 
-  $(document).ready(function port() {
-   (function port() {
+   (function() {
 
-   	if ($('#about-page').is(":visible")) {
-			console.log("vis");
-			$(window).load(function(){
+		$(window).load(function(){
 
-				// container
-				var $container = $('#portfolio-items');
+			// container
+			var $container = $('#portfolio-items');
 
-				function filter_projects(tag)
-				{
-				  // filter projects
-				  $container.isotope({ filter: tag });
-				  // clear active class
-				  $('li.active').removeClass('active');
-				  // add active class to filter selector
-				  $('#portfolio-filter').find("[data-filter='" + tag + "']").parent().addClass('active');
-				  // update location hash
-				  if (tag!='*')
-					window.location.hash+=tag.replace(substr(5),'aboutall');
-				  if (tag=='*')
-				  	window.location.hash+='#aboutall';
-				}
+			function filter_projects(tag)
+			{
+			  // filter projects
+			  $container.isotope({ filter: tag });
+			  // clear active class
+			  $('li.active').removeClass('active');
+			  // add active class to filter selector
+			  $('#portfolio-filter').find("[data-filter='" + tag + "']").parent().addClass('active');
+			  // update location hash
+			  if (tag!='*')
+				window.location.hash=tag.replace('.','');
+			  if (tag=='*')
+			  	window.location.hash='';
+			}
 
-				if ($container.length) {
+			if ($container.length) {
 
-					// convert data-tags to classes
-					$('.project').each(function(){
-						$this = $(this);
-						var tags = $this.data('tags');
-						if (tags) {
-							var classes = tags.split(',');
-							for (var i = classes.length - 1; i >= 0; i--) {
-								$this.addClass(classes[i]);
-							};
-						}
-					})
-
-					// initialize isotope
-					$container.isotope({
-					  // options...
-					  itemSelector : '.project',
-					  layoutMode   : 'fitRows'
-					});
-
-					// filter items
-					$('#portfolio-filter li a').click(function(){
-						var selector = $(this).attr('data-filter');
-						filter_projects(selector);
-						return false;
-					});
-
-					// filter tags if location.has is available. e.g. http://example.com/work.html#design will filter projects within this category
-					if (window.location.hash!='#aboutall')
-					{
-						filter_projects( '.' + window.location.hash.replace('#aboutall','') );
+				// conver data-tags to classes
+				$('.project').each(function(){
+					$this = $(this);
+					var tags = $this.data('tags');
+					if (tags) {
+						var classes = tags.split(',');
+						for (var i = classes.length - 1; i >= 0; i--) {
+							$this.addClass(classes[i]);
+						};
 					}
-				}
-			})
-		} else {
-			console.log("not");
-		}
-	})();
-  });
+				})
 
+				// initialize isotope
+				$container.isotope({
+				  // options...
+				  itemSelector : '.project',
+				  layoutMode   : 'fitRows'
+				});
+
+				// filter items
+				$('#portfolio-filter li a').click(function(){
+					var selector = $(this).attr('data-filter');
+					filter_projects(selector);
+					return false;
+				});
+
+				// filter tags if location.has is available. e.g. http://example.com/work.html#design will filter projects within this category
+				if (window.location.hash!='')
+				{
+					filter_projects( '.' + window.location.hash.replace('#','') );
+				}
+			}
+		})
+
+	})();
 
 
 /* --------------------------------------------------------	
