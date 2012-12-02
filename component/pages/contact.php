c08a794beec39b15eccd0b246866ce67bfffbd13
Chris Frisina <github@specialorange.org>
2012-12-02 17:07:47 -0500

    Refinement of CSS

--- component/pages/contact.php
+++ component/pages/contact.php
@@ -7,7 +7,7 @@
 
 		<div class="container hero-unit">
 			<div class="ac">
-				<p>South Africa, Congo, Nigeria, U.S</p> 
+				<p> you can include another map, a email form, and/or just contact info, your choice</p> 
 			</div>
 		</div>
 
